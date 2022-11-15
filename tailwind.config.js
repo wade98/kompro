f0e637ebd980155f1e2dc6e7c9eee79e80c762c0
wade98 <lolewade@gmail.com>
2022-11-15 13:26:16 -0300

    feat: template is gone, mui is back in town

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	purge: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-	darkMode: true, // or 'media' or 'class'
-	theme: {
-		extend: {
-			height: theme => ({
-				'112': '28rem',
-				'120': '30rem',
-			}),
-			minHeight: theme => ({
-				'80': '20rem',
-			}),
-			colors: {
-				palette: {
-					lighter: '#F5F3FF',
-					light: '#DDD6FE',
-					primary: '#5B21B6',
-					dark: '#4C1D95',
-				},
-			},
-			fontFamily: {
-				primary: ['"Josefin Sans"']
-			}
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [
-		require("@tailwindcss/forms")({
-			strategy: 'class',
-		}),
-	],
-}
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  important: '#__next',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
